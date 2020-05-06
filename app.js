9280aad97e2a911d7087baa3672198d2b67ede54
nicolapirruccio <54145857+nickpirruccio@users.noreply.github.com>
2020-05-06 01:39:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let result = '';
 // Creates and formats the api query
 function createUrl() {
 	const queryFieldValue = inputSearch.value.split(" ").join("+");
-	const API_KEY = config.API_KEY; // ADD YOUR API KEY FROM GOOGLE
+	const API_KEY = config.API_KEY; 
 	return `https://www.googleapis.com/books/v1/volumes?q=${queryFieldValue}&maxResults=10&keyes&key=${API_KEY}`;
 }
 
